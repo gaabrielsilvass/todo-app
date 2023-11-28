2cccf015c7617d4168c444e4ca0da0247438ed47
Gabriel de Souza Silva <gaabrielsilvas17@gmail.com>
2023-11-28 08:43:02 -0300

    exibição das tarefas da página home concluída

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ app.get('/', (requisicao, resposta) => {
                 completa: dado.completa === 0 ? false : true
             }
         })
-    })
 
-    resposta.render('home')
+        resposta.render('home', { tarefas })
+    })
 })
 
 const conexao = mysql.createConnection({
